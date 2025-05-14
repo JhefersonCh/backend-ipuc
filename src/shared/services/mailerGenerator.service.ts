e38c9816afef16d845eff3a68d7bdfe15a6cd2fb
Jairo-18 <jhonlegarda1.2@gmail.com>
2025-05-14 16:23:19 -0500

    Ajust

--- src/shared/services/mailerGenerator.service.ts
+++ src/shared/services/mailerGenerator.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { MailTemplateService } from './mail-template.service';
 import { MailsService } from './mails.service';
-import { User } from '../entities/user.entity';
 
 @Injectable()
 export class MailerGeneratorService {
@@ -9,25 +8,6 @@ export class MailerGeneratorService {
     private readonly mailTemplateService: MailTemplateService,
     private readonly mailService: MailsService,
   ) {}
-
-  async assignTaskMail(user: User) {
-    const body = this.mailTemplateService.assignTaskTemplate(user);
-    await this.mailService.sendEmail({
-      subject: 'Tarea asignada',
-      body,
-      to: user.email,
-    });
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async withTasksPendingMail(payload: any) {
-    const body = this.mailTemplateService.withTasksPendingTemplate(payload);
-    await this.mailService.sendEmail({
-      subject: 'Tareas pendientes',
-      body,
-      to: payload.email,
-    });
-  }
 
   async sendPqrEmail(email: string, name: string, body: string) {
     await this.mailService.sendEmail({
